c3ec87a0b69b71cf225cb394f812139869c9dab0
Guinea Wheek <guineawheek@gmail.com>
2024-05-13 01:01:54 -0700

    copypaste in stmhal-esque crc32 functions

--- src/crc.rs
+++ src/crc.rs
@@ -3,6 +3,9 @@ CRC registers.
 
 */
 
+use core::mem::MaybeUninit;
+use core::ptr::copy_nonoverlapping;
+
 use crate::pac::{Crc,Rcc};
 use crate::rcc::{Enable,Reset};
 
@@ -125,6 +128,57 @@ impl Crc32Engine {
             self.regs.crc32dat().write(|w| unsafe {w.crc32dat().bits(*word)});
         }
         self.regs.crc32dat().read().crc32dat().bits()
+    }
+
+    pub fn init(&mut self) {
+        self.regs.crc32ctrl().write(|w| w.reset().set_bit());
+    }
+
+    pub fn update(&mut self, data: &[u32]) -> u32 {
+        for word in data {
+            self.regs.crc32dat().write(|w| unsafe {
+                w.crc32dat().bits(*word)
+            });
+        }
+        self.regs.crc32dat().read().bits()
+    }
+
+    pub fn update_bytes(&mut self, data: &[u8]) -> u32 {
+
+        let chunks = data.chunks_exact(4);
+        let remainder = chunks.remainder();
+
+                // For each full chunk of four bytes...
+        chunks.for_each(|chunk| unsafe {
+            // Create an uninitialized scratch buffer. We make it uninitialized
+            // to avoid re-zeroing this data inside of the loop.
+            let mut scratch: MaybeUninit<[u8; 4]> = MaybeUninit::uninit();
+
+            // Copy the (potentially unaligned) bytes from the input chunk to
+            // our scratch bytes. We cast the `scratch` buffer from a `*mut [u8; 4]`
+            // to a `*mut u8`.
+            let src: *const u8 = chunk.as_ptr();
+            let dst: *mut u8 = scratch.as_mut_ptr().cast::<u8>();
+            copy_nonoverlapping(src, dst, 4);
+
+            // Mark the scratch bytes as initialized, and then convert it to a
+            // native-endian u32. Feed this into the CRC peripheral
+            self.regs.crc32dat().write(|w| w.bits(u32::from_ne_bytes(scratch.assume_init())));
+        });
+        // If we had a non-multiple of four bytes...
+        if !remainder.is_empty() {
+            // Create a zero-filled scratch buffer, and copy the data in
+            let mut scratch = [0u8; 4];
+
+            // NOTE: We are on a little-endian processor. This means that copying
+            // the 0..len range fills the LEAST significant bytes, leaving the
+            // MOST significant bytes as zeroes
+            scratch[..remainder.len()].copy_from_slice(remainder);
+            self.regs.crc32dat().write(|w| unsafe {w.bits(u32::from_ne_bytes(scratch))});
+        }
+
+        self.regs.crc32dat().read().bits()
+
     }
 
 }
